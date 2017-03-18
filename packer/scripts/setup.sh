f6dbd81b6b843d70ae4a365a4f819249d4583bb9
Matthias Vandermaesen <matthias@colada.be>
2017-03-18 17:10:54 +0100

    added: inject vagrant insecure keys while packing box

--- packer/scripts/setup.sh
+++ packer/scripts/setup.sh
@@ -3,3 +3,14 @@
 # Add vagrant user to sudoers.
 echo "vagrant        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
+
+# Install necessary dependencies
+apt-get -y -q install curl wget git vim
+
+# Installing vagrant keys
+mkdir /home/vagrant/.ssh
+chmod 700 /home/vagrant/.ssh
+cd ~/.ssh
+wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O authorized_keys
+chmod 600 /home/vagrant/.ssh/authorized_keys
+chown -R vagrant /home/vagrant/.ssh
